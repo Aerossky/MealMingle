b39767f33078a0f210a40140269bfff88e4b6003
Reynaldo <stefanusr4@gmail.com>
2023-11-11 21:07:11 +0700

    frontend-Register View Done

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/signin', function () {
     return view('auth.signin');
 });
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
