acaecce87ef2ef3a3501929b79f37e60e739388e
Aerossky <riskyradeonrx560@gmail.com>
2023-11-11 15:52:03 +0700

    frontend - header done

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,41 +1,45 @@
-
- <header class="py-3 lg:pt-6 lg:pb-14 bg-pink-200 font-Montserrat">
-     <div class="container mx-auto lg:relative flex flex-col lg:flex-row lg:justify-between  lg:gap-y-0">
-         {{-- Left Section --}}
-         <div class="flex justify-center lg:justify-normal">
-             <a href="http://">
-                 <img src="{{ asset('img/logo.png') }}" alt="" srcset="" width="150px">
-             </a>
-         </div>
-
-         {{-- Middle Section --}}
-         <div class="flex hidden lg:block items-center justify-center">
-             <nav>
-                 <ul class="flex space-x-4 text-white">
-                     <li><a href="#" class="hover:text-gray-300">HOME</a></li>
-                     <li><a href="#" class="hover:text-gray-300">MENU</a></li>
-                     <li><a href="#" class="hover:text-gray-300">ABOUT US</a></li>
-                     <li><a href="#" class="hover:text-gray-300">FAQ</a></li>
-                 </ul>
-             </nav>
-         </div>
-
-         {{-- Right Section --}}
-         <div class="flex items-center justify-center space-x-4">
-             <div class="text-black">
-                 <!-- Icon Cart -->
-                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor">
-                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                         d="M4 4h16a1 1 0 011 1v12a3 3 0 01-3 3H7a3 3 0 01-3-3V5a1 1 0 011-1zM4 8h16M4 12h16"></path>
-                 </svg>
-             </div>
-
-             <div class="text-black font-medium">
-                 <!-- Text Sign In -->
-                 <a href="#" class="hover:text-gray-300">Sign In</a>
-             </div>
-         </div>
-     </div>
-
- </header>
+<nav class="fixed top-0 z-20 w-full  dark:bg-gray-900 start-0 dark:border-gray-600">
+    <div class="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
+        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="{{ asset('img/logo.png') }}" class="h-14 lg:h-16" alt="Flowbite Logo">
+        </a>
+        <div class="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
+            <button type="button"
+                class="px-4 py-2 text-sm text-center text-black font-bold bg-kuningMM rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get
+                started</button>
+            <button data-collapse-toggle="navbar-sticky" type="button"
+                class="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-sticky" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M1 1h15M1 7h15M1 13h15" />
+                </svg>
+            </button>
+        </div>
+        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+            <ul
+                class="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                    <a href="#"
+                        class="block px-3 py-2 text-white bg-merahMM rounded md:bg-transparent md:text-merahMM font-extrabold  md:p-0 md:dark:text-slate-500"
+                        aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="block px-3 py-2 text-merahMM hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-800 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Menu</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="block px-3 py-2 text-merahMM hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-800 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About
+                        Us</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="block px-3 py-2 text-merahMM hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-800 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">FAQ</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
