638e4cb97dc9824a6bc23038e3c109c09ef932fe
Jovan Louis Lemuel <jovanlemuel9103@gmail.com>
2023-11-13 16:20:41 +0700

    backend - create CRUD tenant

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Menu;
 use App\Models\Tenant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class TenantController extends Controller
 {
@@ -12,7 +15,10 @@ class TenantController extends Controller
      */
     public function index()
     {
-        //
+        $tenants = Tenant::select('id', 'nama_tenant', 'deskripsi', 'user_id')
+            ->orderBy('id', 'asc');
+
+        return view('member.tenant.tenant', ['tenants' => $tenants]);
     }
 
     /**
@@ -20,7 +26,7 @@ class TenantController extends Controller
      */
     public function create()
     {
-        //
+        return view('member.tenant.tenant-add');
     }
 
     /**
@@ -28,38 +34,109 @@ class TenantController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'nama_tenant' => 'required',
+            'deskripsi' => 'required',
+            'user_id' => 'required',
+            // 'picture' => 'required|image|mimes:jpg,png,jpeg|max:2048'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        // $newName = '';
+        // if ($request->file('picture')) {
+        //     $extension = $request->file('picture')->getClientOriginalExtension();
+        //     $newName = $request->email . '-' . now()->timestamp . '.' . $extension;
+        //     $request->file('picture')->storeAs('user_picture', $newName);
+        // }
+
+        $validatedData = $validator->validated();
+        // $validatedData['picture'] = $newName;
+        Tenant::create($validatedData);
+
+        return redirect()->route('tenant.index')->with('status', 'Data berhasil ditambahkan!');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Tenant $tenant)
+    public function show(string $id)
     {
-        //
+        $tenant = Tenant::findOrFail($id);
+        $tenantId = $id;
+
+        $menus = Menu::select('id', 'foto_produk', 'nama_makanan', 'deskripsi', 'harga_produk', 'tenant_id')
+            ->orderBy('id', 'asc')
+            ->where('tenant_id', $id)
+            ->paginate(5);
+        return view('member.tenant.detail', ['tenant' => $tenant, 'menus' => $menus, 'tenantId' => $tenantId]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Tenant $tenant)
+    public function edit(string $id)
     {
-        //
+        $tenant = Tenant::findOrFail($id);
+        return view('member.tenant.tenant-edit', ['tenant' => $tenant]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Tenant $tenant)
+    public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'nama_tenant' => 'required',
+            'deskripsi' => 'required',
+            'user_id' => 'required',
+            // 'picture' => 'required|image|mimes:jpg,png,jpeg|max:2048'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        // if ($request->file('picture')) {
+        //     $extension = $request->file('picture')->getClientOriginalExtension();
+        //     $newName = $request->email . '-' . now()->timestamp . '.' . $extension;
+        //     $request->file('picture')->storeAs('user_picture', $newName);
+        // } else {
+        //     $newName = null;
+        // }
+
+        $tenant = Tenant::findOrFail($id);
+
+        // if ($user->picture && $newName) {
+        //     Storage::disk('public')->delete('user_picture/' . $user->picture);
+        // }
+
+        $tenant->update([
+            'nama_tenant' => $request->nama_tenant,
+            'deskripsi' => $request->deskripsi,
+            'user_id' => $request->user_id,
+            // 'picture' => $newName ?: $user->picture,
+        ]);
+
+        return redirect()->route('tenant.index')->with('status', 'Tenant berhasil diperbarui!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tenant $tenant)
+    public function destroy(string $id)
     {
-        //
+        $tenant = Tenant::findOrFail($id);
+        $tenant->delete();
+
+        return redirect()->route('tenant.index')->with('status', 'Tenant berhasil dihapus!');
     }
 }
