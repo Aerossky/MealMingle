88b6748d1d8c45d7e90a05ee36aa71c528f5c6c5
Jovan Louis Lemuel <jovanlemuel9103@gmail.com>
2023-11-25 12:38:04 +0700

    backend - update menu model with hari & kategori_id

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -15,12 +15,18 @@ class Menu extends Model
       'nama_makanan',
       'deskripsi',
       'harga_produk',
+      'hari',
       'foto_produk',
       'tenant_id',
+      'kategori_id',
     ];
 
     public function tenant(){
         return $this->belongsTo(Tenant::class);
+    }
+
+    public function kategori(){
+        return $this->belongsTo(Kategori::class);
     }
 
     public function keranjang(){
