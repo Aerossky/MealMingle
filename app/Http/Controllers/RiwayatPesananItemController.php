e918b1160674f6e6e19b3bed2ba54a83623a10d2
Jovan Louis Lemuel <jovanlemuel9103@gmail.com>
2023-11-28 00:28:12 +0700

    backend - create riwayat pesanan item controller functions

--- app/Http/Controllers/RiwayatPesananItemController.php
+++ app/Http/Controllers/RiwayatPesananItemController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\RiwayatPesananItem;
+use App\Models\Menu;
 use Illuminate\Http\Request;
+use App\Models\RiwayatPesanan;
+use App\Models\RiwayatPesananItem;
+use Illuminate\Support\Facades\Validator;
 
 class RiwayatPesananItemController extends Controller
 {
@@ -12,15 +15,28 @@ class RiwayatPesananItemController extends Controller
      */
     public function index()
     {
-        //
+        $riwayat_pesanan_items = RiwayatPesananItem::select('id', 'jumlah', 'harga_item', 'riwayat_pesanan_id')
+            ->orderBy('id', 'asc')
+            ->get();
+
+        return view('admin.riwayatpesanan.riwayatpesanan-detail', ['riwayat_pesanan_items' => $riwayat_pesanan_items]);
     }
+
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        $riwayat_pesanans = RiwayatPesanan::select('id', 'transaction_status')
+            ->orderBy('id', 'asc')
+            ->get();
+
+        $menus = Menu::select('id', 'nama_makanan')
+            ->orderBy('id', 'asc')
+            ->get();
+
+        return view('admin.riwayatpesanan.riwayatpesanan-detail', ['riwayat_pesanans' => $riwayat_pesanans, 'menus' => $menus]);
     }
 
     /**
@@ -28,38 +44,97 @@ class RiwayatPesananItemController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'jumlah' => 'required',
+            'harga_item' => 'required',
+            'riwayat_pesanan_id' => 'required',
+            'menu_id' => 'required|array',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $validatedData = $validator->validated();
+
+        $riwayat_pesanan_item = RiwayatPesananItem::create($validatedData);
+        $riwayat_pesanan_item->menu()->attach($request->input('menu_id'));
+
+        return redirect()->route('riwayatpesanan.index')->with('status', 'Data berhasil ditambahkan!');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(RiwayatPesananItem $riwayatPesananItem)
+    public function show($id)
     {
-        //
+        $riwayat_pesanan_item = RiwayatPesananItem::findOrFail($id);
+        $riwayat_pesanan_item_Id = $id;
+
+        return view('admin.riwayatpesanan.riwayatpesanan-detail', ['riwayat_pesanan_item' => $riwayat_pesanan_item, 'riwayat_pesanan_item_Id' => $riwayat_pesanan_item_Id]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(RiwayatPesananItem $riwayatPesananItem)
+    public function edit(string $id)
     {
-        //
+
+        $riwayat_pesanans = RiwayatPesanan::select('id', 'transaction_status')
+            ->orderBy('id', 'asc')
+            ->get();
+
+        $menus = Menu::select('id', 'nama_makanan')
+            ->orderBy('id', 'asc')
+            ->get();
+
+        $riwayat_pesanan_item = RiwayatPesananItem::findOrFail($id);
+        return view('admin.riwayatpesanan.riwayatpesanan-edit', ['riwayat_pesanan_item' => $riwayat_pesanan_item, 'riwayat_pesanans' => $riwayat_pesanans, 'menus' => $menus]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, RiwayatPesananItem $riwayatPesananItem)
+    public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'jumlah' => 'required',
+            'harga_item' => 'required',
+            'riwayat_pesanan_id' => 'required',
+            'menu_id' => 'required|array',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $riwayat_pesanan_item = RiwayatPesananItem::findOrFail($id);
+
+        $riwayat_pesanan_item->update([
+            'jumlah' => $request->jumlah,
+            'harga_item' => $request->harga_item,
+            'riwayat_pesanan_id' => $request->riwayat_pesanan_id,
+        ]);
+
+        $riwayat_pesanan_item->menu()->sync($request->input('menu_id'));
+
+        return redirect()->route('riwayatpesanan.index')->with('status', 'Riwayat pesanan item berhasil diperbarui!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(RiwayatPesananItem $riwayatPesananItem)
+    public function destroy(string $id)
     {
-        //
+        $riwayat_pesanan_item = RiwayatPesananItem::findOrFail($id);
+        $riwayat_pesanan_item->delete();
+
+        return redirect()->route('riwayatpesanan.index')->with('status', 'Riwayat pesanan item berhasil diperbarui!');
     }
 }
