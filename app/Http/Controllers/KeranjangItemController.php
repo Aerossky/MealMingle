8e398b94fe187896f04e64e0c55da6f0e7347ca5
Jovan Louis Lemuel <jovanlemuel9103@gmail.com>
2023-12-01 18:03:55 +0700

    backend - update keranjang item controller

--- app/Http/Controllers/KeranjangItemController.php
+++ app/Http/Controllers/KeranjangItemController.php
@@ -43,38 +43,100 @@ class KeranjangItemController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'jumlah' => 'required',
+            'harga_item' => 'required',
+            'note_item' => 'required',
+            'keranjang_id' => 'required',
+            'menu_id' => 'required|array',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $validatedData = $validator->validated();
+
+        $keranjang_item = KeranjangItem::create($validatedData);
+        $keranjang_item->menu()->attach($request->input('menu_id'));
+
+        return redirect()->route('keranjang.index')->with('status', 'Data berhasil ditambahkan!');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(KeranjangItem $keranjangItem)
+    public function show($id)
     {
-        //
+        $keranjang_item = KeranjangItem::findOrFail($id);
+        $keranjang_item_Id = $id;
+
+        return view('admin.keranjang.keranjang-detail', ['keranjang_item' => $keranjang_item, 'keranjang_item_Id' => $keranjang_item_Id]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(KeranjangItem $keranjangItem)
+    public function edit(string $id)
     {
-        //
+
+        $keranjangs = Keranjang::select('id', 'user_id')
+            ->orderBy('id', 'asc')
+            ->get();
+
+        $menus = Menu::select('id', 'nama_makanan')
+            ->orderBy('id', 'asc')
+            ->get();
+
+        $keranjang_item = KeranjangItem::findOrFail($id);
+        return view('admin.keranjang.keranjang-edit', ['keranjang_item' => $keranjang_item, 'keranjangs' => $keranjangs, 'menus' => $menus]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, KeranjangItem $keranjangItem)
+    public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'jumlah' => 'required',
+            'harga_item' => 'required',
+            'note_item' => 'required',
+            'keranjang_id' => 'required',
+            'menu_id' => 'required|array',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $keranjang_item = KeranjangItem::findOrFail($id);
+
+        $keranjang_item->update([
+            'jumlah' => $request->jumlah,
+            'harga_item' => $request->harga_item,
+            'note_item' => $request->note_item,
+            'keranjang_id' => $request->keranjang_id,
+        ]);
+
+        $keranjang_item->menu()->sync($request->input('menu_id'));
+
+        return redirect()->route('keranjang.index')->with('status', 'Keranjang item berhasil diperbarui!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(KeranjangItem $keranjangItem)
+    public function destroy(string $id)
     {
-        //
+        $keranjang_item = KeranjangItem::findOrFail($id);
+        $keranjang_item->delete();
+
+        return redirect()->route('keranjang.index')->with('status', 'Keranjang item berhasil diperbarui!');
     }
 }
