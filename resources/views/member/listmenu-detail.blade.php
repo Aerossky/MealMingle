2131ae0ecb3ccea87314a677c9a8705c6d5449cc
Aerossky <riskyradeonrx560@gmail.com>
2023-12-04 17:23:35 +0700

    Update listmenu-detail.blade.php

--- resources/views/member/listmenu-detail.blade.php
+++ resources/views/member/listmenu-detail.blade.php
@@ -3,9 +3,9 @@
 
 @section('content')
     <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
-    <section class="mx-auto my-auto px-5 dark:bg-gray-900 w-auto h-auto">
+    <section class="mx-auto my-auto px-5 dark:bg-gray-900 w-auto h-auto py-5">
 
-        <div class="flex flex-col md:flex-row max-w-lg md:max-w-full md:gap-4 md:p-2">
+        <div class="flex flex-col xl:flex-row max-w-lg md:max-w-full md:gap-4 md:p-2">
 
             <!-- Product Image -->
             <div class="flex w-full h-auto bg-gray-200 rounded-2xl md:mx-auto md:col-span-4">
@@ -16,10 +16,10 @@
             <!-- Product Details -->
             <div class="flex flex-col max-w-2xl mx-auto my-auto py-2 md:mx-auto md:col-span-4">
                 <!-- Product Name -->
-                <h2 class="text-2xl font-extrabold text-gray-900 mt-4">Product Name</h2>
+                <h2 class="text-2xl font-extrabold text-gray-900 mt-4">Nama Produk</h2>
 
                 <!-- Tenant Name -->
-                <h2 class="text-lg font-normal italic text-gray-400 mb-4">By Papa Joe</h2>
+                <h2 class="text-lg font-normal italic text-gray-400 mb-4">By Nama Tenant</h2>
 
                 <!-- Product Description -->
                 <div class="text-gray-700 mx-auto my-auto">
@@ -42,7 +42,7 @@
                         et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
                         ut
                         aliquip ex ea commodo
-\
+
                         consequat.
                     </p>
                 </div>
@@ -51,7 +51,7 @@
 
             {{-- add to cart --}}
             <div
-                class="flex flex-col w-full md:w-1/3 justify-items-start my-auto md:mt-3 md:border md:rounded-xl md:p-4 md:col-span-4">
+                class="flex flex-col w-full xl:w-1/3 justify-items-start my-auto md:mt-3 md:border md:rounded-xl md:p-4 md:col-span-4">
                 <div class="invisible md:visible justify-start my-auto mb-4">
                     <label for="addToCart" class="font-bold">Atur Jumlah dan Catatan</label>
                 </div>
@@ -78,25 +78,17 @@
                         <span class="text-xl font-bold">20000</span>
                         <span class="text-xl font-bold">,00</span>
                     </div>
-
-
-                    <!-- Add to Cart Button -->
-                    <button class="bg-merahMM hover:bg-red-800 text-white px-4 py-2 rounded-lg duration-300">
-                        Add to Cart
-                    </button>
-                    <div class="my-2"></div>
-                    <a href="{{ url()->previous() }}"
-                        class="text-center bg-transparent border border-merahMM text-merahMM hover:bg-merahMM hover:text-white px-4 py-2 rounded-lg transition-colors duration-300">
-                        Kembali
-                    </a>
-
-
                 </div>
 
                 <!-- Add to Cart Button -->
-                <button class="bg-merahMM hover:bg-red-700 text-white px-4 py-2 rounded-lg">
+                <button class="bg-merahMM hover:bg-red-700 text-white px-4 py-2 rounded-lg duration-300">
                     Add to Cart
                 </button>
+                <div class="my-2"></div>
+                <a href="{{ url()->previous() }}"
+                    class="text-center bg-transparent border border-merahMM text-merahMM hover:bg-merahMM hover:text-white px-4 py-2 rounded-lg transition-colors duration-300">
+                    Kembali
+                </a>
             </div>
 
         </div>
