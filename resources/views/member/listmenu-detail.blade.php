c852bc2c8a862556bc0572b35bc5a7c0e0aea814
Aerossky <riskyradeonrx560@gmail.com>
2023-12-10 23:47:00 +0700

    Update listmenu-detail.blade.php

--- resources/views/member/listmenu-detail.blade.php
+++ resources/views/member/listmenu-detail.blade.php
@@ -6,11 +6,10 @@
     <section class="mx-auto my-auto px-5 dark:bg-gray-900 w-auto h-auto py-5 ">
 
         <div class="flex flex-col xl:flex-row max-w-lg md:max-w-full md:gap-4 md:p-2">
-
             <!-- Product Image -->
-            <div class="flex  h-auto bg-gray-200 rounded-2xl md:col-span-4  xl:w-1/3">
+            <div class=" rounded-2xl md:col-span-4  xl:w-1/3">
                 <img src="{{ asset('storage/menu/' . $menu->foto_produk) }}" alt="Foto Produk"
-                    class="mx-auto my-auto w-full h-96 object-cover rounded hover:scale-110 transition duration-500 cursor-pointer ">
+                    class="my-auto w-full object-cover rounded cursor-pointer ">
             </div>
 
             <!-- Product Details -->
@@ -28,20 +27,6 @@
                     </p>
                 </div>
 
-                {{-- pilihan hari --}}
-                <div class="flex flex-row justify-start my-auto mb-4">
-                    <label for="pilihHari" class="mr-2 font-bold">Pilih Hari:</label>
-                    <select name="pilihHari" id="pilihHari" class="w-32 p-2 border border-gray-300 rounded">
-                        @foreach ($menu->jadwal_pengiriman as $jadwal)
-                            <option value="{{ $jadwal->hari }}">{{ $jadwal->hari }}, {{ $jadwal->waktu }}</option>
-                        @endforeach
-                        {{-- <option value="Senin">Senin</option>
-                        <option value="Selasa">Selasa</option>
-                        <option value="Rabu">Rabu</option>
-                        <option value="Kamis">Kamis</option>
-                        <option value="Jumat">Jumat</option> --}}
-                    </select>
-                </div>
             </div>
 
             {{-- add to cart --}}
@@ -58,6 +43,27 @@
                         <label for="jumlah" class="mr-2 font-bold">Jumlah:</label>
                         <input type="number" id="jumlah" name="jumlah" min="1" value="1"
                             class="w-16 p-2 border border-gray-300 rounded">
+                    </div>
+                    {{-- pilihan hari --}}
+                    <div class="mb-4">
+                        <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Pilih Hari:</h3>
+                        <ul
+                            class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white flex-wrap">
+                            @foreach ($menu->jadwal_pengiriman as $jadwal)
+                                <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                    <div class="flex items-center ps-3">
+                                        <input id="horizontal-list-radio-license" type="radio" value=""
+                                            name="list-radio"
+                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                        <label for="horizontal-list-radio-license"
+                                            class="w-full py-3 ms-2 text-sm font-medium text-gray-600 dark:text-gray-300">{{ $jadwal->hari }},
+                                            {{ $jadwal->waktu }}</label>
+                                    </div>
+                                </li>
+                            @endforeach
+
+                        </ul>
+
                     </div>
 
                     <!-- Notes Section -->
@@ -93,6 +99,5 @@
         </div>
 
     </section>
-
     @include('layouts.footer')
 @endsection
